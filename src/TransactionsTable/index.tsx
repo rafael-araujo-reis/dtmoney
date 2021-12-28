f49a0197293fd031773094f26eeeffc69175ee92
Rafael Reis <rafa.araujoreis@gmail.com>
2021-12-28 08:59:43 -0300

    Import table and styled table

--- src/TransactionsTable/index.tsx
+++ src/TransactionsTable/index.tsx
@@ -3,7 +3,34 @@ import { Container } from "./styles";
 export function TransactionsTable() {
     return (
         <Container>
-
+            <table>
+                <thead>
+                    <th>Item</th>
+                    <th>Valor</th>
+                    <th>Categoria</th>
+                    <th>Data</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Freelance Doce Imaculada</td>
+                        <td className="entrada">R$450,00</td>
+                        <td>Freelance</td>
+                        <td>15/10/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Salário</td>
+                        <td className="entrada">R$5.100,00</td>
+                        <td>Salário</td>
+                        <td>29/10/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Curso Rocketseat</td>
+                        <td className="saida">-R$798,88</td>
+                        <td>Estudos</td>
+                        <td>20/12/2021</td>
+                    </tr>
+                </tbody>
+            </table>
         </Container>
     );
 }
--- src/TransactionsTable/styles.ts
+++ src/TransactionsTable/styles.ts
@@ -1,3 +1,37 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`    
+    table{
+        border-spacing: 0 0.5rem;
+        width: 100%;
+
+        thead th{
+            padding: 1rem 2rem;
+            text-align: left;
+
+            color: var(--text-body);
+            font-weight: 400;
+            line-height: 1.5rem;
+        }
+
+        tbody{
+            background-color: var(--shape);
+
+            td{
+                padding: 1rem 2rem;
+                color: var(--text-body);
+
+                &:first-child{
+                    color: var(--text-title);
+                    font-weight: 500;
+                }
+                &.entrada{
+                    color: var(--green);
+                }
+                &.saida{
+                    color: var(--red);
+                }
+            }
+        }
+    }
+`
